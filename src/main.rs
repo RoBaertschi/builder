322636785dcaadfa190b777b912e46e4b16f1770
robin <robinbaertschi2@gmail.com>
2022-10-18 21:48:23 +0200

    Changed from Checked to Applicaton Data for later flexebility.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::sync::Arc;
+
 use druid::{PlatformError, Widget, widget::{
     Flex, RadioGroup, BackgroundBrush, Checkbox
 }, AppLauncher, WindowDesc, Data, Color, WidgetExt};
 
-#[derive(Data, Clone, PartialEq, Eq, PartialOrd, Ord)]
-struct RadioData {
-    checked: bool
+#[derive(Data, Clone, PartialEq)]
+struct ApplicationData {
+    checked: Arc<Checked>
 }
 
 #[derive(Data, Clone, PartialEq)]
@@ -35,23 +37,22 @@ impl From<Checked> for bool {
 
 fn main() -> Result<(), PlatformError> {
 
-    let _data: RadioData = RadioData {
-        checked: false
+    let data: ApplicationData = ApplicationData {
+        checked: Arc::new(Checked::False)
     };
 
-    let checked = Checked::True;
+    let _checked = Checked::True;
 
     AppLauncher::with_window(WindowDesc::new(build_ui()).title("Hello World"))
-        .launch(checked)?;
+        .launch(data)?;
         Ok(())
     }
 
 
-fn build_ui() -> impl Widget<Checked> {
-    // shitty: Do Not USE! let data = RadioData {checked: false};
+fn build_ui() -> impl Widget<ApplicationData> {
     
-    let background: BackgroundBrush<Checked> = BackgroundBrush::Color(Color::grey(0.7));
-    let radio_group = RadioGroup::column(vec!(("Hello World", Checked::False), ("Hello World2", Checked::True)));
+    let background: BackgroundBrush<ApplicationData> = BackgroundBrush::Color(Color::grey(0.7));
+    let radio_group = RadioGroup::column(vec!(("Hello World", ApplicationData {checked: Arc::new(Checked::False)}), ("Hello World2", ApplicationData {checked: Arc::new(Checked::True)})));
 
     // TODO: Get _checkboxes into the Flex layout.
     let _checkboxes: Checkbox = Checkbox::new("Hello World");
