29d37eec7b701aa6c3b063a5ac6e98924c9c7294
Robin <rtmbaertschi007@gmail.com>
2022-10-19 13:32:34 +0200

    Added padding to label.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use druid::{PlatformError, Widget, widget::{
-    Flex, RadioGroup, BackgroundBrush, Checkbox, Label
+    Flex, RadioGroup, BackgroundBrush, Checkbox, Label, Padding
 }, AppLauncher, WindowDesc, Data, Color, WidgetExt};
 
 #[derive(Data, Clone, PartialEq)]
@@ -52,12 +52,19 @@ fn build_ui() -> impl Widget<ApplicationData> {
     
     let background: BackgroundBrush<ApplicationData> = BackgroundBrush::Color(Color::grey(0.7));
     let radio_group = RadioGroup::column(vec![("Hello World", ApplicationData {checked: Arc::new(Checked::False)}), ("Hello World2", ApplicationData {checked: Arc::new(Checked::True)})]);
-
+    let mut label = Label::new("Hello");
+    
+    // Set Size
+    label.set_text_size(30.0);
+    let label = label.padding(5.0);
     // TODO: Get _checkboxes into the Flex layout.
     let _checkboxes: Checkbox = Checkbox::new("Hello World");
 
-    Flex::column()
-        .with_child(radio_group)
-        .with_child(Label::new("Hello"))
-        .background(background)
+    let mut layout = Flex::column();
+    layout = layout.with_child(label);
+    layout = layout.with_child(radio_group);
+    
+
+    // Last Line: Attention, please put any layout code before this!
+    layout.background(background)
 }
